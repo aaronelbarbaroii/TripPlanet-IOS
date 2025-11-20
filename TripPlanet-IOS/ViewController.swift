542f861b3be2e64d85bf82d5d0418f2674c463c4
aaron <aaronelbarbaroii@gmail.com>
2025-11-20 10:53:02 +0100

    Initial Commit

---
+++ TripPlanet-IOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TripPlanet-IOS
+//
+//  Created by Mananas on 20/11/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
